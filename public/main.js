f91d1cc483266c45e33fde115bf3c3cd184acd2f
Konstantin <2447785@gmail.com>
2016-09-28 15:27:56 +0300

    Init project

---
+++ public/main.js
@@ -0,0 +1,67 @@
+(function () {
+	'use strict';
+
+	if (typeof window === 'object') {
+
+		//import
+		let Button = window.Button;
+		let Chat = window.Chat;
+		let Form = window.Form;
+
+		let loginPage = document.querySelector('.js-login');
+		let chatPage = document.querySelector('.js-chat');
+
+		let form = new Form({
+			el: document.createElement('div'),
+			data: {
+				title: 'Login',
+				fields: [
+					{
+						name: 'user',
+						type: 'text'
+					},
+					{
+						name: 'email',
+						type: 'email'
+					}
+				],
+				controls: [
+					{
+						text: 'Войти',
+						attrs: {
+							type: 'submit'
+						}
+					}
+				]
+			}
+		});
+
+		let chat = new Chat({
+			el: document.createElement('div'),
+		});
+
+		form.on('submit', event => {
+			event.preventDefault();
+
+			let formData = form.getFormData();
+			technolibs.request('/api/login', formData);
+
+			chat.set({
+				username: formData.user,
+				email: formData.email
+			})
+			.render();
+
+			chat.subscribe();
+
+			loginPage.hidden = true;
+			chatPage.hidden = false;
+		});
+
+		loginPage.appendChild(form.el);
+		chatPage.appendChild(chat.el);
+
+		loginPage.hidden = false;
+	}
+
+})();
